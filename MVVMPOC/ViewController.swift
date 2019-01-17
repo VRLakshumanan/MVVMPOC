17eab19f6177d0324693ba97a3545f0a4a25257f
CSS CORP <csscorp@CSSs-Mac-mini.local>
2019-01-17 12:44:25 +0530

    Initial Commit

---
+++ MVVMPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMPOC
+//
+//  Created by CSS CORP on 17/01/19.
+//  Copyright © 2019 CSS CORP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
